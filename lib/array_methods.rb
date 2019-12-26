565cce44783069aaffee1f806a7756d9688efa76
cesar mota <cesarmota@ymail.com>
2019-12-26 21:48:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,13 @@
 def find_element_index(array, value_to_find)
- 
-  p  array.index(value_to_find)
+ counter=0
+ while counter < array.length do
+   if array[counter]==value_to_find
+     return counter
+  #array.index(value_to_find)
+end
+counter += 1
+end
+nil
 end
 
 
@@ -11,7 +18,10 @@ def find_max_value(array)
 end
 
 
+
+
 def find_min_value(array)
   array.reverse!
   p array.last
+  
 end
